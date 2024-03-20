ef7be4b86f5b8d9f9e61ec00fc845214e2d5eaa5
MUG <ys8267928563@outlook.jp>
2024-03-20 17:49:29 +0900

    uitoolkit開発

---
+++ Assets/Scripts/Test/CardDemo.cs
@@ -0,0 +1,49 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UIToolkitExamplesB
+{
+    public class CardDemo : VisualElement
+    {
+        private Label m_characterName;
+        private Label m_description;
+        private VisualElement m_thumbnail;
+
+        private string CharacterName => m_characterName.text;
+        private string Description => m_description.text;
+
+        public new class UxmlFactory : UxmlFactory<CardDemo, UxmlTraits> { }
+
+        public new class UxmlTraits : VisualElement.UxmlTraits
+        {
+            public UxmlStringAttributeDescription _characterName = new UxmlStringAttributeDescription { name = "character-name", defaultValue = "Name"};
+            public UxmlStringAttributeDescription _description = new UxmlStringAttributeDescription { name = "description", defaultValue = "Description"};
+            // public UxmlAssetAttributeDescription<Texture2D> _thumbnail = new() { name = "thumbnail"};
+
+            public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+            {
+                base.Init(ve, bag, cc);
+                var card = ve as CardDemo;
+                card.m_characterName.text = _characterName.GetValueFromBag(bag, cc);
+                card.m_description.text = _description.GetValueFromBag(bag, cc);
+                // card.m_thumbnail.style.backgroundImage = _thumbnail.GetValueFromBag(bag, cc);
+            }
+        }
+
+        public CardDemo()
+        {
+            var treeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI Toolkit/Card.uxml");
+            var container = treeAsset.Instantiate();
+            hierarchy.Add(container);
+
+            m_characterName = container.Q<Label>("Name");
+            m_description = container.Q<Label>("Dexcription");
+            m_thumbnail = container.Q<VisualElement>("Thumbnail");
+
+            var card = container.Q<VisualElement>("Root");
+            card.AddManipulator(new Clickable(() => Debug.Log("Clicked!!")));
+        }
+    }
+}
+
